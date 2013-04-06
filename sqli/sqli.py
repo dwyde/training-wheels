f3c5699375934e4bc165a7d0bb9eb16f4a9bb824
David Wyde <david.wyde@gmail.com>
2013-04-06 13:29:43 -0500

    Consolidate SQL injection code.

--- sqli/sqli.py
+++ sqli/sqli.py
@@ -1,21 +1,45 @@
 import sqlite3
 
-conn = sqlite3.connect(':memory:')
-cur = conn.cursor()
 
-cur.execute('''CREATE TABLE users (name text)''')
-cur.execute('''INSERT INTO users VALUES ('david')''')
-cur.execute('''INSERT INTO users VALUES ('foo')''')
-conn.commit()
+def init_database():
+    """ Initialize an in-memory SQLite database of users. """
+    conn = sqlite3.connect(':memory:')
+    with conn:
+        conn.execute('''CREATE TABLE users (name text)''')
+        conn.execute('''INSERT INTO users VALUES ('david')''')
+        conn.execute('''INSERT INTO users VALUES ('foo')''')
+    return conn
 
-name = raw_input('Retrieve data on at least one user: ')
-cur.execute("SELECT * FROM users WHERE name = '%s'" % name)
+def sql_select_injection(name):
+    """ Perform an SQL SELECT query that's vulnerable to injection.
+    
+    The return value indicates whether multiple rows were in the result.
+    """
+    # name = "' OR '1'='1"
+    conn = init_database()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE name = '%s'" % name)
+        results = cur.fetchall()
+        cur.close()
+    
+    success = len(results) == 2
+    return success
 
-results = cur.fetchall()
-print results
-if len(results) == 2:
-    print 'Success!'
-else:
-    print 'Nope!'
-
-# ' OR '1'='1
+def sql_insert_injection(name):
+    """ Allow multiple statements to be executed via SQL injection.
+    
+    Returns success if a new row was inserted as part of the query.
+    """
+    # name = "'; INSERT INTO users VALUES ('zz')--"
+    conn = init_database()
+    with conn:
+        cursor = conn.cursor()
+        cursor.executescript("SELECT * FROM users WHERE name='%s'" % name)
+        cursor.execute('SELECT * FROM users')
+        results = cursor.fetchall()
+        cursor.close()
+        
+    print results
+    success = len(results) > 2
+    return success
